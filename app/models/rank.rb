7152c6c0837982988ebf8928d4853bfe6ffc2f84
MitchGianoni <mitchgianoni@yahoo.com>
2016-04-16 18:23:10 -0400

    fixed some paperclip shit

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,9 +1,7 @@
 class Rank < ActiveRecord::Base
 	has_many :users
-
-	#When S3 and AWS are active, uncomment the code below
 	
-	#has_attached_file :image, styles: { thumb: "50x50>" medium: "500x500>" },
-	#	default_url: "images/:style/missing.png"
-	#validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+	has_attached_file :image, styles: { thumb: "50x50>", medium: "500x500>" },
+		default_url: "images/:style/missing.png"
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
