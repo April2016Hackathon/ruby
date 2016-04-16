727534aa699294aeefeb24a7b36876ca9ae14381
Pat McAleavey <pmcaleavey8@gmail.com>
2016-04-16 12:52:14 -0400

    create method for postings controller and jbuilder in postings

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -1,17 +1,38 @@
 class PostingsController < ApplicationController
-	before_action :authenticate!, only[:create, :destroy]
+	before_action :authenticate!, only:[:create, :destroy]
 
 	#this code needs to be reviewed and reworked
+
+
+
+  # def index
+  #   @posts = Post.all
+  #   render "index.json.jbuilder", status: :ok
+  # end
+
+
+  def create
+      @post = current_user.postings.create(user_id: session[:user_id], text: params[:text], title: params[:title],##describe
+                       mood: params[:mood])
+  #binding.pry
+    if @post.save
+      render "create.json.jbuilder", status: :created
+    else
+      render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+
 
 # 	def create
 # 		@post = current_user.posts.create(
 # 			#need to define the parameters to create a post
 # 			)
 # 		if @post.save
-# 			render "create.json.jbuilder", 
+# 			render "create.json.jbuilder",
 # 				status: :created
 # 		else
-# 			render json: { errors: @post.errors.full_messages }, 
+# 			render json: { errors: @post.errors.full_messages },
 # 				status: :unprocessible_entity
 # 		end
 # 	end
@@ -35,3 +56,4 @@ class PostingsController < ApplicationController
 # 		end
 # 	end
 # end
+end
--- config.ru
+++ config.ru
@@ -3,11 +3,11 @@
 require ::File.expand_path('../config/environment', __FILE__)
 run Rails.application
 
-require 'rack/cors' 
-use Rack::Cors do 
+require 'rack/cors'
+use Rack::Cors do
 	allow do
 		origins '*'
-		resource '*'
+		resource '*',
 			:headers => :any,
 			:methods => [:get, :post, :delete, :put, :options]
 	end
