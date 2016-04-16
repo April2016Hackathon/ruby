cd4a6498da0ffa25f5ea68cf6870ec058a280ae1
MitchGianoni <mitchgianoni@yahoo.com>
2016-04-16 13:19:23 -0400

    removed mood column from user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416151118) do
+ActiveRecord::Schema.define(version: 20160416171728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 20160416151118) do
     t.string   "username"
     t.string   "password_digest"
     t.string   "email"
-    t.boolean  "mood"
     t.integer  "wildpoints",      default: 0
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
