e8089f232fcc2ece6ce8a4884880d1d6c54997b4
MitchGianoni <mitchgianoni@yahoo.com>
2016-04-16 15:04:20 -0400

    added code and jbuilder file to send postings index array to front end

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -2,46 +2,21 @@ class PostingsController < ApplicationController
 	before_action :authenticate!, only:[:create, :destroy]
 	#this code needs to be reviewed and reworked
 
-
-
-  # def index
-  #   @posts = Post.all
-  #   render "index.json.jbuilder", status: :ok
-  # end
+   def index
+     @posts = Posting.all
+     render "index.json.jbuilder", status: :ok
+  end
 
 
   def create
       @post = current_user.postings.create(user_id: session[:user_id], text: params[:text], title: params[:title],##describe
                        mood: params[:mood])
-  #binding.pry
     if @post.save
       render "create.json.jbuilder", status: :created
     else
       render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
     end
   end
-
-
-
-# 	def create
-# 		@post = current_user.posts.create(
-# 			#need to define the parameters to create a post
-# 			)
-# 		if @post.save
-# 			render "create.json.jbuilder",
-# 				status: :created
-# 		else
-# 			render json: { errors: @post.errors.full_messages },
-# 				status: :unprocessible_entity
-# 		end
-# 	end
-
-# 	def index
-# 		@posts = Post.all
-# 		@users = User.all
-# 		render "index.json.jbuilder"
-# 				status: :ok
-# 	end
 
 # 	def destroy
 # 		@post = Post.find_by(id: params["id"])
---
+++ app/views/postings/index.json.jbuilder
@@ -0,0 +1,8 @@
+json.array! @posts do |post|
+	json.user post.user.username
+	json.title post.title
+	json.text post.text
+	json.mood post.mood
+	json.id post.id
+	json.created_at post.created_at
+end
